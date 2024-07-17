c68859557c87cc7aca74280e5f76a94d08c99ae9
JZomber <103089013+JZomber@users.noreply.github.com>
2024-07-17 19:21:11 -0300

    Build Upd

--- [Dungeon] PrograSistemas/Assets/Scripts/ScriptableObjects/PlayerCharacterData.cs
+++ [Dungeon] PrograSistemas/Assets/Scripts/ScriptableObjects/PlayerCharacterData.cs
@@ -1,35 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Animations;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "PlayerCharacterData", menuName = "ScriptableObjects/Character/Player Character Data", order = 1)]
 public class PlayerCharacterData : ScriptableObject
 {
     [Header("Rogue data")]
-    [SerializeField] private AnimatorController rogueAnimator;
+    [SerializeField] private RuntimeAnimatorController rogueAnimator;
     [SerializeField] private int rogueMaxLife;
     [SerializeField] private PowerUpData rogueStarterPowerUp;
 
-    public AnimatorController GetRogueAnimator => rogueAnimator;
+    public RuntimeAnimatorController GetRogueAnimator => rogueAnimator;
     public int GetRogueMaxLife => rogueMaxLife;
     public PowerUpData GetRogueStarterPowerUp => rogueStarterPowerUp;
     
     [Header("Knight data")]
-    [SerializeField] private AnimatorController knightAnimator;
+    [SerializeField] private RuntimeAnimatorController knightAnimator;
     [SerializeField] private int knightMaxLife;
     [SerializeField] private PowerUpData knightStarterPowerUp;
-    
-    public AnimatorController GetKnightAnimator => knightAnimator;
+
+    public RuntimeAnimatorController GetKnightAnimator => knightAnimator;
     public int GetKnightMaxLife => knightMaxLife;
     public PowerUpData GetKnightStarterPowerUp => knightStarterPowerUp;
     
     [Header("Mage data")]
-    [SerializeField] private AnimatorController mageAnimator;
+    [SerializeField] private RuntimeAnimatorController mageAnimator;
     [SerializeField] private int mageMaxLife;
     [SerializeField] private PowerUpData mageStarterPowerUp;
-    
-    public AnimatorController GetMageAnimator => mageAnimator;
+
+    public RuntimeAnimatorController GetMageAnimator => mageAnimator;
     public int GetMageMaxLife => mageMaxLife;
     public PowerUpData GetMageStarterPowerUp => mageStarterPowerUp;
 }
